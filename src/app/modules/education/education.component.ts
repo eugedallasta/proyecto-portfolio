ade7f34b7cc83ba1df15c8eea9b5743483f95a48
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-17 20:40:21 -0300

    education section buttons

--- src/app/modules/education/education.component.ts
+++ src/app/modules/education/education.component.ts
@@ -27,4 +27,8 @@ export class EducationComponent implements OnInit {
       },
     });
   }
+
+  public onOpenModal(mode:String, education?:Educacion):void{
+    console.log('onOpenModal', mode, education);
+  }
 }
