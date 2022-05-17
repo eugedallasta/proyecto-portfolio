610884133ae6a05e41256832a09dd856f5a24d17
Eugenia Dallasta <mariaedallasta@gmail.com>
2022-05-17 16:11:07 -0300

    Add Education Service

---
+++ src/app/models/educacion.ts
@@ -0,0 +1,10 @@
+export interface Educacion {
+  idEdu: number;
+  tituloEdu: string;
+  fechaEdu: string;
+  descripcionEdu: string;
+  imagenEdu: string;
+}
+
+
+
--- src/app/modules/education/education.component.ts
+++ src/app/modules/education/education.component.ts
@@ -1,15 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Educacion } from 'src/app/models/educacion';
+import { EducationService } from 'src/app/services/education.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  public education: Educacion[] = [];
 
-  constructor() { }
+  constructor(private educationService: EducationService) {}
 
   ngOnInit(): void {
+    this.getEducation();
   }
 
+  getEducation(): void {
+    this.educationService.getEducation().subscribe({
+      next: (response: Educacion[]) => {
+        this.education = response;
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+      },
+    });
+  }
 }
